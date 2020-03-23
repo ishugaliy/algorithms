63586dd10d0ea4e2005222c69f0fb1723e9e874b
Iurii Shugalii <iurii.shugalii@slidepresenter.com>
2020-03-23 16:02:04 +0200

    solve rabbit jump = dp

--- src/dp/RabbitJump.java
+++ src/dp/RabbitJump.java
@@ -1,33 +1,101 @@
 package dp;
 
-import java.util.ArrayList;
-
 public class RabbitJump {
 
     public static void main(String[] args) {
-        int[] p1 = new int[] {0, 1, 2, 0, 0, 0, 0}; // first el - sentinel
-        System.out.println(countJumpOn(3, p1, p1.length - 1));
+        System.out.println("jump on                  : "    // 8
+                + countJumpOn(2, new int[]{0, 1, 2, 0, 0, 0, 0}, 5));
+
+        System.out.println("jump in                  : "    // 3
+                + countJumpIn(4, new int[]{0, 1, 1, 0, 0, 0, 0, 0, 0}, 8));
+
+        System.out.println("jump on (wolves)         : "    // 2
+                + countJumpOnWithWolves(2, new int[]{0, 1, 2, 0, 0, 0, 0}, new int[]{0, 0, 1, 0, 0, 0, 0}, 5));
+
+        System.out.println("jump on (frogs)          : "    // 12
+                + countJumpOnWithFrogs(2, new int[]{0, 1, 1, 0, 0, 0, 0, 0, 0}, new int[] {0, 1, 1, 0, 0, 1, 10, 1, 0}, 6));
     }
 
     /**
-     * Rabbit can make jump up on pillars. At once, it can do up to {k} jump length.
-     * Calculate amount of all possible ways to get to {n} pillar.
+     * There are {n} pillars and white rabbit. Rabbit can jump-on from 1 to K pillars.
+     * Calculate amount of all possible ways to get to last pillar.
      *
-     * @param k - max number of jump length
+     * @param k - jump on length from 1 to k
      * @param p - pillars
      * @param n - target pillar to jump on
-     * @return number of all ways to get to the {n} pillar
+     * @return number of all ways to get to the last pillar
      */
     public static int countJumpOn(int k, int[] p, int n) {
-        if (p[n] != 0) {
-            return p[n];
-        }
-        new ArrayList<Integer>().stream()
-                .mapToInt(i -> i).toArray();
+        if (p[n] != 0) return p[n];
         int to = Math.min(k, n);
         for (int i = 1; i <= to; i++) {
             p[n] += countJumpOn(k, p, n - i);
         }
+        return p[n];
+    }
+
+    /**
+     * There are {n} pillars and white rabbit. Rabbit can jump-in from 2 to K pillars.
+     * Calculate amount of all possible ways to get to last pillar.
+     *
+     * @param k - jump on length from 2 to k
+     * @param p - pillars
+     * @param n - target pillar to jump on
+     * @return number of all ways to get to the last pillar
+     */
+    public static int countJumpIn(int k, int[] p, int n) {
+        if (p[n] != 0) return p[n];
+        int to = Math.min(k, n);
+        for (int i = 2; i <= to; i++) {
+            if (n % i == 0) {
+                p[n] += countJumpIn(k, p, n / i);
+            }
+        }
+        return p[n];
+    }
+
+    /**
+     * There are {n} pillars and white rabbit. Rabbit can jump-on from 1 to K pillars.
+     * There may be a wolfe sitting on a pillar.
+     * Calculate amount of all possible ways to get to last pillar, avoiding pillars with wolves.
+     *
+     * @param k - jump on length from 1 to k
+     * @param p - pillars
+     * @param w - wolves positions. Values: 0 - no wolfe, 1 - there is a wolfe
+     * @param n - target pillar to jump on
+     * @return number of all ways to get to the last pillar
+     */
+    public static int countJumpOnWithWolves(int k, int[] p, int[] w, int n) {
+        if (w[n] == 1) return 0; // wolfe here, 0 ways to get here.
+        if (p[n] > 0) return p[n];
+        int to = Math.min(k, n);
+        for (int i = 1; i <= to; i++) {
+            p[n] += countJumpOnWithWolves(k, p, w,n - i);
+        }
+        return p[n];
+    }
+
+    /**
+     * There are {n} pillars and white rabbit. Rabbit can jump-on from 1 to K pillars.
+     * There is a frog sitting on each pillar that give a reward to a rabbit for jumping on its pillar.
+     * Calculate maximum amount of money white rabbit can receive to get to the last pillar.
+     *
+     * @param k - jump on length from 1 to k
+     * @param p - pillars
+     * @param f - frogs reward
+     * @param n - target pillar to jump on
+     * @return number of all ways to get to the last pillar
+     */
+    public static int countJumpOnWithFrogs(int k, int[] p, int[] f, int n) {
+        if (p[n] > 0) return p[n];
+        int to = Math.min(k, n);
+        for (int i = 1; i <= to; i++) {
+            int v = countJumpOnWithFrogs(k, p, f,n - i);
+            if (v > p[n]) {
+                p[n] = v;
+            }
+        }
+        p[n] += f[n];
         return p[n];
     }
 }
--- src/leetcode/contest/weekly/_181/ValidPath.java
+++ src/leetcode/contest/weekly/_181/ValidPath.java
@@ -1,34 +1,22 @@
 package leetcode.contest.weekly._181;
 
+import java.util.*;
+
 // TODO: solve
 // https://leetcode.com/contest/weekly-contest-181/problems/check-if-there-is-a-valid-path-in-a-grid/
 public class ValidPath {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().hasValidPath(new int[][] {{2,4,3}, {6,5,2}}));
+        System.out.println(new Solution().hasValidPath(new int[][] {{2,4,3}, {6,5,2}}));                         // true
+        System.out.println(new Solution().hasValidPath(new int[][] {{1,2,1}, {1,2,1}}));                         // false
+        System.out.println(new Solution().hasValidPath(new int[][] {{1,1,2}}));                                  // false
+        System.out.println(new Solution().hasValidPath(new int[][] {{1,1,1,1,1,1,3}}));                          // true
+        System.out.println(new Solution().hasValidPath(new int[][] {{2},{2},{2},{2},{2},{2},{2},{6}}));          // true
     }
 
     static class Solution {
 
         public boolean hasValidPath(int[][] grid) {
-            int row = 0;
-            int col = 0;
-            while (row < grid.length && col < grid[row].length && grid[row][col] > 0) {
-                switch (grid[row][col]) {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                    case 6:
-                        break;
-                }
-            }
             return false;
         }
     }
