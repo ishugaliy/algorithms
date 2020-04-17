ecb47c83efe281bcf4bca550765dcf6af364127f
Iurii Shugalii <iurii.shugalii@slidepresenter.com>
2020-04-17 16:11:38 +0300

    solve ShoutingDistance

--- src/hackerrank/OptimalTrapPlacementOnTree.java
+++ src/hackerrank/OptimalTrapPlacementOnTree.java
@@ -3,10 +3,7 @@ package hackerrank;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 // https://www.hackerrank.com/contests/projector-algo-base-8-hw-5-zpun0n6c/challenges/optimal-trap-placement-tree
 @SuppressWarnings("all")
@@ -22,9 +19,24 @@ public class OptimalTrapPlacementOnTree {
         tree.add(3, new ArrayList<>(Arrays.asList(2, 5)));
         tree.add(4, new ArrayList<>(Arrays.asList(2)));
         tree.add(5, new ArrayList<>(Arrays.asList(3)));
-        System.out.println(maxTrapValue(tree, values));
+        System.out.println(maxTrapValue(tree, values, new boolean[values.length]));
 
-        // CASE #2: 10
+        // CASE #2: 16
+        values = new int[]{0, 2, 3, 4, -5, 4, -6, 5, 7};
+        tree = new ArrayList<>();
+        tree.add(0, Collections.emptyList());
+        tree.add(1, new ArrayList<>(Arrays.asList(3)));
+        tree.add(2, new ArrayList<>(Arrays.asList(3)));
+        tree.add(3, new ArrayList<>(Arrays.asList(1, 2, 4)));
+        tree.add(4, new ArrayList<>(Arrays.asList(3, 5, 6)));
+        tree.add(5, new ArrayList<>(Arrays.asList(4)));
+        tree.add(6, new ArrayList<>(Arrays.asList(4, 7)));
+        tree.add(7, new ArrayList<>(Arrays.asList(6, 8)));
+        tree.add(8, new ArrayList<>(Arrays.asList(7)));
+        System.out.println(maxTrapValue(tree, values, new boolean[values.length]));
+
+
+        // CASE #3: 10
         values = new int[]{0, 3, 10, 3, 3};
         tree = new ArrayList<>();
         tree.add(0, Collections.emptyList());
@@ -33,9 +45,9 @@ public class OptimalTrapPlacementOnTree {
         tree.add(3, new ArrayList<>(Arrays.asList(2)));
         tree.add(4, new ArrayList<>(Arrays.asList(2)));
 
-        System.out.println(maxTrapValue(tree, values));
+        System.out.println(maxTrapValue(tree, values, new boolean[values.length]));
 
-        // CASE #3: 9
+        // CASE #4: 9
         values = new int[]{0, 3, 8, 3, 3};
         tree = new ArrayList<>();
         tree.add(0, Collections.emptyList());
@@ -43,7 +55,7 @@ public class OptimalTrapPlacementOnTree {
         tree.add(2, new ArrayList<>(Arrays.asList(1, 3, 4)));
         tree.add(3, new ArrayList<>(Arrays.asList(2)));
         tree.add(4, new ArrayList<>(Arrays.asList(2)));
-        System.out.println(maxTrapValue(tree, values));
+        System.out.println(maxTrapValue(tree, values, new boolean[values.length]));
 
         System.exit(0);
 
@@ -54,37 +66,44 @@ public class OptimalTrapPlacementOnTree {
             List<List<Integer>> _tree = initTree(_n);
             readTree(_tree, reader);
 
-            System.out.println(maxTrapValue(_tree, _values));
+            System.out.println(maxTrapValue(_tree, _values, new boolean[_values.length]));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static long maxTrapValue(List<List<Integer>> tree, int[] values) {
-        return Math.max(
-                _maxTrapValue(tree, 1, false, values, new boolean[values.length]),
-                _maxTrapValue(tree, 1, true, values, new boolean[values.length])
-        );
+    public static long maxTrapValue(List<List<Integer>> tree, int[] values, boolean[] visited) {
+        // traverse tree for all graphes starting with and witout trap
+        long value = 0;
+        for (int i = 1; i < tree.size(); i++) {
+            if (!visited[i]) {
+                value += Math.max(
+                        dfs(tree, i, false, values, Arrays.copyOf(visited, visited.length)),
+                        dfs(tree, i, true, values, visited)
+                );
+            }
+        }
+        return value;
     }
 
-    private static long _maxTrapValue(List<List<Integer>> tree, Integer node,
-                                      boolean withTrap, int[] values, boolean[] visited) {
-        if (visited[node]) return 0;
+    private static long dfs(List<List<Integer>> tree, Integer node,
+                            boolean withTrap, int[] values, boolean[] visited) {
+        if (visited[node] || values[node] < 0) return 0;
 
         visited[node] = true;
-        long v = withTrap ? values[node] : 0;
+        long value = withTrap ? values[node] : 0;
         List<Integer> children = tree.get(node);
         for (Integer child : children) {
-            v += _maxTrapValue(tree, child, !withTrap, values, visited);
+            value += dfs(tree, child, !withTrap, values, visited);
         }
-        return v;
+        return value;
     }
 
     private static int[] readValues(int n, String readLine) {
         int[] v = new int[n + 1];
         String[] raw = readLine.split(" ");
         for (int i = 0; i < raw.length; i++) {
-            v[i+1] = Integer.parseInt(raw[i]);
+            v[i + 1] = Integer.parseInt(raw[i]);
         }
         return v;
     }
--- src/hackerrank/ShoutingDistance.java
+++ src/hackerrank/ShoutingDistance.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 
 //https://www.hackerrank.com/contests/projector-algo-base-8-hw-5-zpun0n6c/challenges/shouting
@@ -13,33 +14,46 @@ import java.util.Set;
 public class ShoutingDistance {
 
     public static void main(String[] args) {
-        // CASE #1: 4
+        System.out.print("CASE #1: [4] : ");
         System.out.println(minShoutDistance(new HashSet<>(Arrays.asList(
                 new Point(3, 2), new Point(3, 3), new Point(3, 4),
                 new Point(3, 3), new Point(3, 9), new Point(3, 8),
                 new Point(3, 1)
         ))));
 
-        // CASE #2: 7.0710678118654755
+        System.out.print("CASE #2: [7.0710678118654755] : ");
         System.out.println(minShoutDistance(new HashSet<>(Arrays.asList(
                 new Point(5, 0), new Point(0, 5),
                 new Point(-5, 0), new Point(0, -5)
         ))));
 
-        // CASE #3: 0.0
+        System.out.print("CASE #3: [0.0] : ");
         System.out.println(minShoutDistance(new HashSet<>(Arrays.asList(new Point(123, 342)))));
 
-        // CASE #4: 3.162277
+        System.out.print("CASE #4: [3.162277] : ");
         System.out.println(minShoutDistance(new HashSet<>(Arrays.asList(
                 new Point(1, 5), new Point(1, 4),
                 new Point(2, 1), new Point(3, 1)
         ))));
 
-        // CASE #5: 4.12310
+        System.out.print("CASE #5: [4.12310] : ");
         System.out.println(minShoutDistance(new HashSet<>(Arrays.asList(
                 new Point(3, 1), new Point(3, 6),
                 new Point(4, 5), new Point(5, 6)
         ))));
+
+        System.out.print("CASE #6: [3] : ");
+        System.out.println(minShoutDistance(new HashSet<>(Arrays.asList(
+                new Point(1, 1), new Point(2, 2), new Point(3, 1),
+                new Point(1, 6), new Point(2, 5), new Point(3, 6)
+        ))));
+
+        System.out.print("CASE #7: [28284.27124] : ");
+        System.out.println(minShoutDistance(new HashSet<>(Arrays.asList(
+                new Point(-10_000, -10_000), new Point(10_000, 10_000)))));
+
+        System.out.print("CASE #8: [0.0] : ");
+        System.out.println(minShoutDistance(new HashSet<>(Arrays.asList(new Point(123, 342), new Point(123, 342)))));
 
 
         System.exit(0);
@@ -61,26 +75,37 @@ public class ShoutingDistance {
     }
 
     public static double minShoutDistance(Set<Point> people) {
-        double minDistance = Double.MAX_VALUE;
-        for (Point screamer : people) {
-            minDistance = Math.min(_minShoutDistance(new HashSet<>(people), screamer), minDistance);
-        }
-        return minDistance;
-    }
-
-    private static double _minShoutDistance(Set<Point> people, Point screamer) {
-        if (!people.remove(screamer)) return 0;
-
-        double minDistance = Double.MAX_VALUE;
-        Point closestReceiver = null;
-        for (Point receiver : people) {
-            double distance = screamer.distance(receiver);
-            if (distance < minDistance) {
-                minDistance = distance;
-                closestReceiver = receiver;
+        if (people.size() == 1) return 0.0;
+        double bad = -1;
+        double good =  28285; // ~ distance between pq p1{x: -10000; y: -10000} and p2{x:  10000;  y: 10000}
+        while (good - bad > 0.000001) {
+            double m = (bad + good) / 2;
+            if(isShoutDistanceValid(people, m)) {
+                good = m;
+            } else {
+                bad = m;
             }
         }
-        minDistance = minDistance != Double.MAX_VALUE ? minDistance : 0;
-        return Math.max(minDistance, _minShoutDistance(people, closestReceiver));
+        return good;
+    }
+
+    private static boolean isShoutDistanceValid(Set<Point> people, double distance) {
+        Set<Point> visited = new HashSet<>();
+        LinkedList<Point> queue = new LinkedList<>();
+
+        Point first = people.stream().findFirst().get();
+        queue.add(first);
+        visited.add(first);
+
+        while (!queue.isEmpty()) {
+            Point screamer = queue.poll();
+            for (Point receiver : people) {
+                if (!visited.contains(receiver) && screamer.distance(receiver) <= distance) {
+                    queue.add(receiver);
+                    visited.add(receiver);
+                }
+            }
+        }
+        return visited.size() == people.size();
     }
 }
